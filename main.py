698cf5bf4079477aa225107343402cbf0bfe7e08
wolv3 <rerussell@protonmail.com>
2022-08-08 19:32:57 +0200

    Initial Commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, redirect, send_file
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+Bootstrap(app)
+
+title = "Reece Russell"
+
+
+@app.route("/")
+def home():
+    return render_template("index.html", title=title)
+
+
+@app.route("/cv")
+def download_cv():
+
+    return send_file("./static/pdf/Reece Russell CV.pdf", as_attachment=True)
+
+
+if __name__ == "__main__":
+    app.run()
